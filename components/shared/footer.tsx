cdb986353e814db4fe429ca935477d8b3c9ab3ce
shvam0000 <shvam0000@gmail.com>
2024-03-27 13:23:15 -0400

    refactor and responsive code

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,45 +1,31 @@
-import React from 'react';
-import Logo from '@/utils/images/logo.png';
-import Image from 'next/image';
-import { IM_Fell_Great_Primer } from 'next/font/google';
-import { SocialIcon } from 'react-custom-social-icons';
-import Link from 'next/link';
+import React from "react";
+import Logo from "@/utils/images/logo.png";
+import Image from "next/image";
+import { IM_Fell_Great_Primer } from "next/font/google";
+import Link from "next/link";
+import { SocialIcon } from "react-social-icons";
+import { SocialMediaLinks } from "@/utils/const";
 
 const IMFellGreatPrimer = IM_Fell_Great_Primer({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 const Footer = () => {
   return (
     <div className={`${IMFellGreatPrimer.className} px-10`}>
-      <div className="flex justify-between items-center">
+      <div className="flex flex-wrap items-center justify-between">
         <Link href="/">
           <figure className="py-5">
             <Image src={Logo} alt="logo" width={300} height={200} />
           </figure>
         </Link>
-        <div className="flex">
-          <Link href="https://www.x.com/">
-            <figure className="px-2 rounded-full">
-              <SocialIcon network="twitter" size="small" />
+        <div className="flex items-center justify-center pb-5 md:pb-0">
+          {SocialMediaLinks.map((link) => (
+            <figure className="px-2" key={link.id}>
+              <SocialIcon url={link.link} style={{ height: 35, width: 35 }} />
             </figure>
-          </Link>
-          <Link href="https://www.facebook.com/">
-            <figure className="text-[#3b5998] px-2 rounded-full">
-              <SocialIcon network="facebook" size="small" />
-            </figure>
-          </Link>
-          <Link href="https://www.linkedin.com/">
-            <figure className="px-2 rounded-full">
-              <SocialIcon network="linkedin" size="small" />
-            </figure>
-          </Link>
-          <Link href="https://www.instagram.com/">
-            <figure className="px-2 rounded-full">
-              <SocialIcon network="instagram" size="small" />
-            </figure>
-          </Link>
+          ))}
         </div>
       </div>
     </div>
--- utils/const.ts
+++ utils/const.ts
@@ -1,92 +1,111 @@
 export const UKToday = [
   {
     id: 1,
-    title: 'News',
-    link: '/uktoday/news',
+    title: "News",
+    link: "/uktoday/news",
   },
   {
     id: 2,
-    title: 'Latest Development',
-    link: '/uktoday/latest-development',
+    title: "Latest Development",
+    link: "/uktoday/latest-development",
   },
 ];
 
 export const Capabilities = [
   {
     id: 1,
-    title: 'Consultancy',
-    link: '/capabilities/consultancy',
+    title: "Consultancy",
+    link: "/capabilities/consultancy",
   },
   {
     id: 2,
-    title: 'Himalayan Haat',
-    link: '/capabilities/himalayan-haat',
+    title: "Himalayan Haat",
+    link: "/capabilities/himalayan-haat",
   },
   {
     id: 3,
-    title: 'Vidya Path',
-    link: '/capabilities/vidyapath',
+    title: "Vidya Path",
+    link: "/capabilities/vidyapath",
   },
 ];
 
 export const AboutUs = [
   {
     id: 1,
-    title: 'Our Story',
-    link: '/about/our-story',
+    title: "Our Story",
+    link: "/about/our-story",
   },
   {
     id: 2,
-    title: 'Our Team',
-    link: '/about/our-team',
+    title: "Our Team",
+    link: "/about/our-team",
   },
 ];
 
 export const UttarakhandInsights = [
   {
     id: 1,
-    title: 'Natives of Uttarakhand',
-    link: '/uttarakhand-insights/natives-of-uttarakhand',
+    title: "Natives of Uttarakhand",
+    link: "/uttarakhand-insights/natives-of-uttarakhand",
   },
   {
     id: 2,
-    title: 'Uttarakhand Today',
-    link: '/uttarakhand-insights/uttarakhand-today',
+    title: "Uttarakhand Today",
+    link: "/uttarakhand-insights/uttarakhand-today",
   },
   {
     id: 3,
-    title: 'Analytics',
-    link: '/uttarakhand-insights/analytics',
+    title: "Analytics",
+    link: "/uttarakhand-insights/analytics",
   },
 ];
 
 export const VoiceOf4th = [
   {
     id: 1,
-    title: 'Blog',
-    link: '/voice-of-fourth/blog',
+    title: "Blog",
+    link: "/voice-of-fourth/blog",
   },
   {
     id: 2,
-    title: 'Editorial',
-    link: '/voice-of-fourth/editorial',
+    title: "Editorial",
+    link: "/voice-of-fourth/editorial",
   },
 ];
 
 export const Team = [
   {
     id: 1,
-    name: 'Samarth Khanna',
-    role: 'Co-Founder',
+    name: "Samarth Khanna",
+    role: "Co-Founder",
   },
   {
     id: 2,
-    name: 'Gargi',
-    role: 'Co-Founder',
+    name: "Gargi",
+    role: "Co-Founder",
   },
   {
     id: 3,
-    name: 'Lorem Ipsum',
-    role: 'Co-Founder',
+    name: "Lorem Ipsum",
+    role: "Co-Founder",
+  },
+];
+
+export const SocialMediaLinks = [
+  {
+    id: 1,
+    link: "https://x.com/",
+  },
+  {
+    id: 2,
+    link: "https://linkedin.com/",
+  },
+  {
+    id: 3,
+    link: "https://facebook.com/",
+  },
+  {
+    id: 4,
+    link: "https://instagram.com/",
   },
 ];
