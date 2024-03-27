a14fe52728cafa4f34a0851a97276b9b4a90d27e
shvam0000 <shvam0000@gmail.com>
2024-03-27 11:29:37 -0400

    🍱 add more images

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -9,6 +9,17 @@ import Picture3 from '@/utils/images/3.jpg';
 import Picture4 from '@/utils/images/4.jpg';
 import Picture5 from '@/utils/images/5.jpg';
 import Picture6 from '@/utils/images/6.jpg';
+import Picture7 from '@/utils/images/7.jpg';
+import Picture8 from '@/utils/images/8.jpg';
+import Picture9 from '@/utils/images/9.jpg';
+import Picture10 from '@/utils/images/10.jpg';
+import Picture11 from '@/utils/images/11.jpg';
+import Picture12 from '@/utils/images/12.jpg';
+import Picture13 from '@/utils/images/13.jpg';
+import Picture14 from '@/utils/images/14.jpg';
+import Picture15 from '@/utils/images/15.jpg';
+import Picture16 from '@/utils/images/16.jpg';
+
 import { KeepInTouch } from '../shared';
 import { Map } from '@/utils/icons';
 
@@ -19,6 +30,19 @@ const IMFellGreatPrimer = IM_Fell_Great_Primer({
 
 const Hero = () => {
   const images = [Picture1, Picture2, Picture3, Picture4, Picture5, Picture6];
+
+  const images2 = [
+    Picture7,
+    Picture8,
+    Picture9,
+    Picture10,
+    Picture11,
+    Picture12,
+    Picture13,
+    Picture14,
+    Picture15,
+    Picture16,
+  ];
 
   return (
     <div className={`${IMFellGreatPrimer.className}`}>
@@ -38,16 +62,33 @@ const Hero = () => {
 
       <Marquee
         duration={15000}
-        background="#fafafa"
-        height="250px"
+        // background="#fafafa"
+        height="200px"
         reverse={true}>
         {images.map((image, index) => (
           <Image
             src={image}
             alt="picsum"
-            width={900}
-            height={250}
+            width={250}
+            height={100}
             key={index}
+            className="hover:scale-110 ease-in-out transform transition duration-500"
+          />
+        ))}
+      </Marquee>
+      <Marquee
+        duration={15000}
+        // background="#fafafa"
+        height="200px"
+        reverse={true}>
+        {images2.map((image, index) => (
+          <Image
+            src={image}
+            alt="picsum"
+            width={250}
+            height={100}
+            key={index}
+            className="hover:scale-110 ease-in-out transform transition duration-500"
           />
         ))}
       </Marquee>
