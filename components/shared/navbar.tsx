89230d05509620218c74d83af3dd43e24eeca4e6
shvam0000 <shvam0000@gmail.com>
2024-03-28 22:28:51 -0400

    🎨 add content to the sliding togglebar

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
-import { IM_Fell_Great_Primer } from "next/font/google";
-import Link from "next/link";
-import Image from "next/image";
+import React, { useState } from 'react';
+import { IM_Fell_Great_Primer } from 'next/font/google';
+import Link from 'next/link';
+import Image from 'next/image';
+import { IoCloseSharp } from 'react-icons/io5';
 
 import {
   Capabilities,
   VoiceOf4th,
   AboutUs,
   UttarakhandInsights,
-} from "@/utils/const";
-import Logo from "@/utils/images/logo.png";
-import { Menu } from "@/utils/icons";
+} from '@/utils/const';
+import Logo from '@/utils/images/logo.png';
+import { Menu } from '@/utils/icons';
 
 const IMFellGreatPrimer = IM_Fell_Great_Primer({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 const Navbar = () => {
@@ -57,15 +58,13 @@ const Navbar = () => {
         <div className="hidden items-center justify-center md:flex">
           <button
             onClick={() => toggleSlidingMenu()}
-            className="block text-5xl font-bold text-[#BA9C57]"
-          >
+            className="block text-5xl font-bold text-[#BA9C57]">
             <Menu />
           </button>
 
           <Link
             className="mx-4 text-justify text-tfp-white-100 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-            href="/"
-          >
+            href="/">
             <figure className="md:mb-3">
               <Image
                 src={Logo}
@@ -78,12 +77,10 @@ const Navbar = () => {
           <div
             className="relative"
             onMouseEnter={toggleCapabilitiesMenu}
-            onMouseLeave={toggleCapabilitiesMenu}
-          >
+            onMouseLeave={toggleCapabilitiesMenu}>
             <Link
               href="/capabilities"
-              className="mx-4 rounded-lg text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl"
-            >
+              className="mx-4 rounded-lg text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl">
               Services
             </Link>
             {isOpenCapabilities && (
@@ -92,8 +89,7 @@ const Navbar = () => {
                   <Link
                     key={item.id}
                     href={item.link}
-                    className="text-tfp-black-100 mb-2 block w-96 text-lg text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-                  >
+                    className="text-tfp-black-100 mb-2 block w-96 text-lg text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                     {item.title}
                   </Link>
                 ))}
@@ -103,12 +99,10 @@ const Navbar = () => {
           <div
             className="relative"
             onMouseEnter={toggleAboutUsMenu}
-            onMouseLeave={toggleAboutUsMenu}
-          >
+            onMouseLeave={toggleAboutUsMenu}>
             <Link
               className="mx-4 text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl"
-              href="/about"
-            >
+              href="/about">
               About Us
             </Link>
             {isAboutUs && (
@@ -117,8 +111,7 @@ const Navbar = () => {
                   <Link
                     key={item.id}
                     href={item.link}
-                    className="text-tfp-black-100 mb-2 block w-96 text-lg text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-                  >
+                    className="text-tfp-black-100 mb-2 block w-96 text-lg text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                     {item.title}
                   </Link>
                 ))}
@@ -128,12 +121,10 @@ const Navbar = () => {
           <div
             className="relative"
             onMouseEnter={toggleUttarakhanInsightsMenu}
-            onMouseLeave={toggleUttarakhanInsightsMenu}
-          >
+            onMouseLeave={toggleUttarakhanInsightsMenu}>
             <Link
               className="mx-4 text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl"
-              href="/uttarakhand-insights"
-            >
+              href="/uttarakhand-insights">
               Insights
             </Link>
             {isUttrakhandInsights && (
@@ -142,8 +133,7 @@ const Navbar = () => {
                   <Link
                     key={item.id}
                     href={item.link}
-                    className="text-tfp-black-100 mb-2 block w-96 text-lg text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-                  >
+                    className="text-tfp-black-100 mb-2 block w-96 text-lg text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                     {item.title}
                   </Link>
                 ))}
@@ -154,12 +144,10 @@ const Navbar = () => {
           <div
             className="relative"
             onMouseEnter={toggleVoiceOf4thMenu}
-            onMouseLeave={toggleVoiceOf4thMenu}
-          >
+            onMouseLeave={toggleVoiceOf4thMenu}>
             <Link
               href="/voice-of-fourth"
-              className="mx-4 rounded-lg text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl"
-            >
+              className="mx-4 rounded-lg text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl">
               Raibar
             </Link>
             {isOpenVoiceOf4th && (
@@ -168,8 +156,7 @@ const Navbar = () => {
                   <Link
                     key={item.id}
                     href={item.link}
-                    className="text-md text-tfp-black-100 mb-2 block text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-                  >
+                    className="text-md text-tfp-black-100 mb-2 block text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                     {item.title}
                   </Link>
                 ))}
@@ -179,34 +166,65 @@ const Navbar = () => {
           {/* End of modified "About" dropdown section */}
           <Link
             className="mx-4 text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl"
-            href="/careers"
-          >
+            href="/careers">
             Careers
           </Link>
           <Link
             className="mx-4 text-justify text-sm text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300 lg:text-2xl"
-            href="/contact-us"
-          >
+            href="/contact-us">
             Contact Us
           </Link>
         </div>
         <div
           className={`fixed inset-0 transform ${
-            isMenuVisible ? "translate-x-0" : "-translate-x-full"
-          } z-50 h-screen w-1/3 bg-white transition-transform duration-300 ease-in-out`}
-        >
+            isMenuVisible ? 'translate-x-0' : '-translate-x-full'
+          } z-50 h-screen w-1/3 bg-tfp-gold-200 transition-transform duration-300 ease-in-out`}>
           {/* Menu content goes here */}
-          <div className="flex justify-around py-96 text-2xl text-tfp-gold-300">
-            Coming Soon
-            <button onClick={toggleSlidingMenu}>X</button>
+          <div
+            onClick={toggleSlidingMenu}
+            className="absolute right-10 top-10 text-2xl text-tfp-blue-100 transform translate hover:scale-125 ease-in-out duration-300 cursor-pointer">
+            <IoCloseSharp />
+          </div>
+          <div className="flex px-10 text-tfp-blue-100 py-16 text-2xl">
+            <div>
+              <Link
+                href="/capabilities"
+                className="block py-2 transition-colors duration-300 ease-in-out hover:text-tfp-white-100">
+                Services
+              </Link>
+              <Link
+                href="/about"
+                className="block py-2  transition-colors duration-300 ease-in-out hover:text-tfp-white-100">
+                About Us
+              </Link>
+              <Link
+                href="/uttarakhand-insights"
+                className="mb-2 block py-2 transition-colors duration-300 ease-in-out hover:text-tfp-white-100">
+                Insights
+              </Link>
+              <Link
+                href="/voice-of-fourth"
+                className="block py-2  transition-colors duration-300 ease-in-out hover:text-tfp-white-100">
+                Raibar
+              </Link>
+              <Link
+                href="/careers"
+                className="block py-2 transition-colors duration-300 ease-in-out hover:text-tfp-white-100">
+                Careers
+              </Link>
+              <Link
+                href="/contact-us"
+                className="block py-2 transition-colors duration-300 ease-in-out hover:text-tfp-white-100">
+                Contact Us
+              </Link>
+            </div>
           </div>
         </div>
         <div className="md:hidden">
           <div className="flex">
             <button
               onClick={toggleMenu}
-              className="block text-3xl font-bold text-[#F4E697]"
-            >
+              className="block text-3xl font-bold text-[#F4E697]">
               <Menu />
             </button>
             <Link href="/" className="mx-10">
@@ -222,38 +240,32 @@ const Navbar = () => {
             <div className="py-2 md:hidden">
               <Link
                 href="/capabilities"
-                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-              >
+                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                 Services
               </Link>
               <Link
                 href="/about"
-                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-              >
+                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                 About Us
               </Link>
               <Link
                 href="/uttarakhand-insights"
-                className="mb-2 block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-              >
+                className="mb-2 block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                 Insights
               </Link>
               <Link
                 href="/voice-of-fourth"
-                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-              >
+                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                 Raibar
               </Link>
               <Link
                 href="/careers"
-                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-              >
+                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                 Careers
               </Link>
               <Link
                 href="/contact-us"
-                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300"
-              >
+                className="block py-2 text-gray-300 transition-colors duration-300 ease-in-out hover:text-tfp-gold-300">
                 Contact Us
               </Link>
             </div>
